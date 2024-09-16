fdb920632476d9d58730ccff3c6d0a4131adcf9b
alanliongar <alan.lucindo.gomes@alumni.usp.br>
2024-09-16 18:01:53 -0300

    2

--- app/src/main/java/com/devspacecomposeinit/MainActivity.kt
+++ app/src/main/java/com/devspacecomposeinit/MainActivity.kt
@@ -6,14 +6,21 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -24,6 +31,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.modifier.modifierLocalMapOf
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight.Companion.SemiBold
 import androidx.compose.ui.tooling.preview.Preview
@@ -44,12 +52,36 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    val artist = Artist(
+                    val leonardo = Artist(
                         "Leonardo Da Vinci",
                         "3 minutes ago",
                         R.drawable.ic_leonardo_da_vinci,
-                        R.drawable.ic_mona_lisa)
-                    ArtistCard(artist)
+                        R.drawable.ic_mona_lisa
+                    )
+                    val picasso = Artist(
+                        "Pablo Picasso",
+                        "5 minutes ago",
+                        R.drawable.ic_pablo_picasso,
+                        R.drawable.ic_beijo
+                    )
+                    val salvador = Artist(
+                        "Salvador Dali",
+                        "7 minutes ago",
+                        R.drawable.ic_salvador_dali,
+                        R.drawable.ic_persistence_of_memory
+                    )
+                    val vanGogh = Artist(
+                        "Vincent Van Gogh",
+                        "10 minutes ago",
+                        R.drawable.ic_vincent_van_gogh,
+                        R.drawable.ic_starry_night
+                    )
+                    val artists = listOf(vanGogh, picasso, salvador, leonardo)
+                    LazyColumn {
+                        items(items = artists) { artist ->
+                            ArtistCard(artist = artist)
+                        }
+                    }
                 }
             }
         }
@@ -58,7 +90,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun ArtistCard(artist: Artist) {
-    Column {
+    Column(modifier = Modifier.padding(8.dp)) {
         Row(verticalAlignment = Alignment.CenterVertically) {
             Image(
                 modifier = Modifier
@@ -74,8 +106,19 @@ fun ArtistCard(artist: Artist) {
                 Text(artist.lastSeenOnline, color = Color.Gray)
             }
         }
-        Card{
-            Image(painter = painterResource(id=artist.art), "Artist Art")
+        Card(
+            modifier = Modifier.padding(8.dp),
+            elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
+        ) {
+            Image(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(200.dp),
+                contentScale = ContentScale.Crop,
+                painter = painterResource(id = artist.art),
+                contentDescription = "Artist Art",
+            )
+
         }
     }
 }
@@ -91,7 +134,12 @@ data class Artist(
 @Composable
 fun ArtistCardPreview() {
     ComposeInitTheme {
-        val artist = Artist("Leonardo Da Vinci", "3 minutes ago", R.drawable.ic_leonardo_da_vinci, R.drawable.ic_mona_lisa)
+        val artist = Artist(
+            "Leonardo Da Vinci",
+            "3 minutes ago",
+            R.drawable.ic_leonardo_da_vinci,
+            R.drawable.ic_mona_lisa
+        )
         ArtistCard(artist)
     }
 }
--- app/src/main/java/com/devspacecomposeinit/anotacoes.kt
+++ app/src/main/java/com/devspacecomposeinit/anotacoes.kt
@@ -3,7 +3,7 @@ package com.devspacecomposeinit
 class anotacoes {
     /*
     * Dia 1 - Row e Column são chaves {} e têm propriedades, imagens e textos tbm
-    *
+    * Dia 1 - Ainda no primeiro dia, tentei implementar o RV, e não deu certo, ao scrolar pra cima dá um erro, fiz uma abertura de issue no github, vamos ver se o roque visualiza.
     * */
 }
-//oieee
+//oieeekkjd
